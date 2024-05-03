70444c7b74c43b4a9a4fcdabb6c4d9fbbb7ca7c0
Jason Merten <jmerte@gmail.com>
2024-05-03 08:57:08 -0400

    updated lazy comment and noice configs

--- nvim-lazy/lua/plugins/comment.lua
+++ nvim-lazy/lua/plugins/comment.lua
@@ -1,22 +1,68 @@
+function _G.__toggle_contextual(vmode)
+	local cfg = require("Comment.config"):get()
+	local U = require("Comment.utils")
+	local Op = require("Comment.opfunc")
+	local range = U.get_region(vmode)
+	local same_line = range.srow == range.erow
+
+	local ctx = {
+		cmode = U.cmode.toggle,
+		range = range,
+		cmotion = U.cmotion[vmode] or U.cmotion.line,
+		ctype = same_line and U.ctype.linewise or U.ctype.blockwise,
+	}
+
+	local lcs, rcs = U.parse_cstr(cfg, ctx)
+	local lines = U.get_lines(range)
+
+	local params = {
+		range = range,
+		lines = lines,
+		cfg = cfg,
+		cmode = ctx.cmode,
+		lcs = lcs,
+		rcs = rcs,
+	}
+
+	if same_line then
+		Op.linewise(params)
+	else
+		Op.blockwise(params)
+	end
+end
+
 return {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    keys = {
-        { "gcc", mode = "n",          desc = "Comment toggle current line" },
-        { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
-        { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
-        { "gbc", mode = "n",          desc = "Comment toggle current block" },
-        { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-        { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
-    },
-    init = function()
-        vim.keymap.set({ "n", "i" }, "<C-c>", function()
-            require("Comment.api").toggle.linewise.current()
-        end, { desc = "Toggle comment" })
-        vim.keymap.set("v", "<C-c>", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-            { desc = "Toggle comment" })
-    end,
-    config = function(_, opts)
-        require("Comment").setup(opts)
-    end,
+	"numToStr/Comment.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>ft", ":TodoTelescope<CR>", desc = "Find TODOs in project" },
+		},
+		opts = {},
+	},
+	init = function()
+		vim.keymap.set({ "n", "i" }, "<C-c>", function()
+			require("Comment.api").toggle.linewise.current()
+		end, { desc = "Toggle comment" })
+		vim.keymap.set(
+			"x",
+			"<C-c>",
+			"<cmd>set operatorfunc=v:lua.__toggle_contextual<CR>g@",
+			{ desc = "Toggle comment" }
+		)
+	end,
+	opts = {
+		post_hook = function()
+			local r = unpack(vim.api.nvim_win_get_cursor(0))
+			local rcnt = vim.api.nvim_buf_line_count(0)
+			if rcnt > r then
+				vim.api.nvim_win_set_cursor(0, { r + 1, 0 })
+			end
+		end,
+	},
+	config = function(_, opts)
+		require("Comment").setup(opts)
+	end,
 }
--- nvim-lazy/lua/plugins/noice.lua
+++ nvim-lazy/lua/plugins/noice.lua
@@ -1,56 +1,59 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        {
-            "rcarriga/nvim-notify",
-            event = "VeryLazy",
-            config = function()
-                require("notify").setup({
-                    background_colour = "#000000",
-                    enabled = false,
-                })
-            end,
-        },
-    },
-    opts = {
-        cmdline = {
-            view = "cmdline",
-        },
-        lsp = {
-            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true,
-            },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-            bottom_search = true,         -- use a classic bottom cmdline for search
-            command_palette = true,       -- position the cmdline and popupmenu together
-            long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false,       -- add a border to hover docs and signature help
-        },
-        routes = {
-            {
-                filter = {
-                    event = "msg_show",
-                    any = {
-                        { find = "%d+L, %d+B" },
-                        { find = "; after #%d+" },
-                        { find = "; before #%d+" },
-                        { find = "%d fewer lines" },
-                        { find = "%d more lines" },
-                    },
-                },
-                opts = { skip = true },
-            },
-        },
-    },
-    config = function(_, opts)
-        require("noice").setup(opts)
-    end,
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			"rcarriga/nvim-notify",
+			event = "VeryLazy",
+			config = function()
+				require("notify").setup({
+					background_colour = "#000000",
+					render = "compact",
+				})
+			end,
+		},
+	},
+	opts = {
+		cmdline = {
+			view = "cmdline",
+		},
+		notify = {
+			enabled = true,
+		},
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+				["vim.lsp.util.stylize_markdown"] = false,
+				["cmp.entry.get_documentation"] = false,
+			},
+		},
+		-- you can enable a preset for easier configuration
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					any = {
+						{ find = "%d+L, %d+B" },
+						{ find = "; after #%d+" },
+						{ find = "; before #%d+" },
+						{ find = "%d fewer lines" },
+						{ find = "%d more lines" },
+					},
+				},
+				opts = { skip = true },
+			},
+		},
+	},
+	config = function(_, opts)
+		require("noice").setup(opts)
+	end,
 }
