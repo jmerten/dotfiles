b305df22b5ea76324b53a8d214260210de22d228
Jason Merten <jmerte@gmail.com>
2024-05-06 07:04:06 -0400

    fixed formatting and removed duplicate vars

--- nvim-lazy/lua/plugins/cmp.lua
+++ nvim-lazy/lua/plugins/cmp.lua
@@ -1,169 +1,166 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- snippet plugin
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			dependencies = "rafamadriz/friendly-snippets",
-			opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-			config = function(_, opts)
-				local nvimdir = vim.fn.stdpath("config")
-				require("luasnip.loaders.from_vscode").lazy_load({
-					paths = {
-						nvimdir .. "/snippets",
-					},
-				})
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        -- snippet plugin
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            dependencies = "rafamadriz/friendly-snippets",
+            opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+            config = function(_, opts)
+                local nvimdir = vim.fn.stdpath("config")
+                require("luasnip.loaders.from_vscode").lazy_load({
+                    paths = {
+                        nvimdir .. "/snippets",
+                    },
+                })
 
-				vim.api.nvim_create_autocmd("InsertLeave", {
-					callback = function()
-						if
-							require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-							and not require("luasnip").session.jump_active
-						then
-							require("luasnip").unlink_current()
-						end
-					end,
-				})
-			end,
-		},
+                vim.api.nvim_create_autocmd("InsertLeave", {
+                    callback = function()
+                        if
+                            require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                            and not require("luasnip").session.jump_active
+                        then
+                            require("luasnip").unlink_current()
+                        end
+                    end,
+                })
+            end,
+        },
 
-		-- autopairing of (){}[] etc
-		{
-			"windwp/nvim-autopairs",
-			opts = {
-				fast_wrap = {},
-				disable_filetype = { "TelescopePrompt", "vim" },
-			},
-			config = function(_, opts)
-				require("nvim-autopairs").setup(opts)
+        -- autopairing of (){}[] etc
+        {
+            "windwp/nvim-autopairs",
+            opts = {
+                fast_wrap = {},
+                disable_filetype = { "TelescopePrompt", "vim" },
+            },
+            config = function(_, opts)
+                require("nvim-autopairs").setup(opts)
 
-				-- setup cmp for autopairs
-				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-				require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-			end,
-		},
+                -- setup cmp for autopairs
+                local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+                require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+            end,
+        },
 
-		-- cmp sources plugins
-		{
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"ray-x/lsp_signature.nvim",
-		},
-		"onsails/lspkind.nvim",
-	},
-	config = function()
-		-- vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+        -- cmp sources plugins
+        {
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "ray-x/lsp_signature.nvim",
+        },
+        "onsails/lspkind.nvim",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local function border(hl_name)
+            return {
+                { "╭", hl_name },
+                { "─", hl_name },
+                { "╮", hl_name },
+                { "│", hl_name },
+                { "╯", hl_name },
+                { "─", hl_name },
+                { "╰", hl_name },
+                { "│", hl_name },
+            }
+        end
 
-		local cmp = require("cmp")
-		local function border(hl_name)
-			return {
-				{ "╭", hl_name },
-				{ "─", hl_name },
-				{ "╮", hl_name },
-				{ "│", hl_name },
-				{ "╯", hl_name },
-				{ "─", hl_name },
-				{ "╰", hl_name },
-				{ "│", hl_name },
-			}
-		end
+        local luasnip = require("luasnip")
 
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+        vim.opt.completeopt = { "menu", "menuone", "noinsert", "noselect" }
 
-		vim.opt.completeopt = { "menu", "menuone", "noinsert", "noselect" }
+        -- See :help cmp-config
+        cmp.setup({
+            window = {
+                completion = {
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+                    scrollbar = false,
+                    col_offset = -3,
+                    side_padding = 0,
+                },
+                documentation = {
+                    border = border("CmpDocBorder"),
+                    winhighlight = "Normal:CmpDoc",
+                },
+            },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
 
-		-- See :help cmp-config
-		cmp.setup({
-			window = {
-				completion = {
-					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-					scrollbar = false,
-					col_offset = -3,
-					side_padding = 0,
-				},
-				documentation = {
-					border = border("CmpDocBorder"),
-					winhighlight = "Normal:CmpDoc",
-				},
-			},
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
+            view = {
+                entries = { name = "custom", selection_order = "near_cursor" },
+            },
 
-			view = {
-				entries = { name = "custom", selection_order = "near_cursor" },
-			},
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                    local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                    kind.kind = " " .. (strings[1] or "") .. " "
+                    kind.menu = "   " .. (strings[2] or "") .. ""
 
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-					local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-					local strings = vim.split(kind.kind, "%s", { trimempty = true })
-					kind.kind = " " .. (strings[1] or "") .. " "
-					kind.menu = "   " .. (strings[2] or "") .. ""
+                    return kind
+                end,
+            },
 
-					return kind
-				end,
-			},
-
-			mapping = {
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-u>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<Esc>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Insert,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-						cmp.select_next_item()
-						-- cmp.confirm { select = true }
-						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-						-- this way you will only jump inside the snippet region
-					elseif require("luasnip").expand_or_locally_jumpable() then
-						vim.fn.feedkeys(
-							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-							""
-						)
-					else
-						fallback()
-					end
-				end, {
-					"i",
-					"s",
-				}),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif require("luasnip").jumpable(-1) then
-						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-					else
-						fallback()
-					end
-				end, {
-					"i",
-					"s",
-				}),
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "path" },
-				{ name = "nvim_lua" },
-				{ name = "luasnip", keyword_legth = 2 },
-				{ name = "buffer", keyword_length = 3 },
-			},
-		})
-	end,
+            mapping = {
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-u>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Esc>"] = cmp.mapping.close(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Insert,
+                    select = true,
+                }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+                        cmp.select_next_item()
+                        -- cmp.confirm { select = true }
+                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                        -- this way you will only jump inside the snippet region
+                    elseif require("luasnip").expand_or_locally_jumpable() then
+                        vim.fn.feedkeys(
+                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+                            ""
+                        )
+                    else
+                        fallback()
+                    end
+                end, {
+                    "i",
+                    "s",
+                }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif require("luasnip").jumpable(-1) then
+                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                    else
+                        fallback()
+                    end
+                end, {
+                    "i",
+                    "s",
+                }),
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip", keyword_legth = 2 },
+                { name = "path" },
+                { name = "nvim_lua" },
+                { name = "buffer",  keyword_length = 3 },
+            },
+        })
+    end,
 }
--- nvim-lazy/lua/plugins/lspconfig.lua
+++ nvim-lazy/lua/plugins/lspconfig.lua
@@ -1,167 +1,165 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		capabilities.textDocument.completion.completionItem = {
-			documentationFormat = { "markdown", "plaintext" },
-			snippetSupport = true,
-			preselectSupport = true,
-			insertReplaceSupport = true,
-			labelDetailsSupport = true,
-			deprecatedSupport = true,
-			commitCharactersSupport = true,
-			tagSupport = { valueSet = { 1 } },
-			resolveSupport = {
-				properties = {
-					"documentation",
-					"detail",
-					"additionalTextEdits",
-				},
-			},
-		}
+        capabilities.textDocument.completion.completionItem = {
+            documentationFormat = { "markdown", "plaintext" },
+            snippetSupport = true,
+            preselectSupport = true,
+            insertReplaceSupport = true,
+            labelDetailsSupport = true,
+            deprecatedSupport = true,
+            commitCharactersSupport = true,
+            tagSupport = { valueSet = { 1 } },
+            resolveSupport = {
+                properties = {
+                    "documentation",
+                    "detail",
+                    "additionalTextEdits",
+                },
+            },
+        }
 
-		-- LSP Display helpers
-		local function lspSymbol(name, icon)
-			local hl = "DiagnosticSign" .. name
-			vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-		end
+        -- LSP Display helpers
+        local function lspSymbol(name, icon)
+            local hl = "DiagnosticSign" .. name
+            vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+        end
 
-		lspSymbol("Error", "󰅙")
-		lspSymbol("Info", "󰋼")
-		lspSymbol("Hint", "󰌵")
-		lspSymbol("Warn", "")
+        lspSymbol("Error", "󰅙")
+        lspSymbol("Info", "󰋼")
+        lspSymbol("Hint", "󰌵")
+        lspSymbol("Warn", "")
 
-		vim.diagnostic.config({
-			virtual_text = {
-				prefix = "",
-			},
-			signs = true,
-			underline = true,
-			update_in_insert = false,
-		})
+        vim.diagnostic.config({
+            virtual_text = {
+                prefix = "",
+            },
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+        })
 
-		-- Borders for LspInfo winodw
-		local win = require("lspconfig.ui.windows")
-		local _default_opts = win.default_opts
+        -- Borders for LspInfo winodw
+        local win = require("lspconfig.ui.windows")
+        local _default_opts = win.default_opts
 
-		win.default_opts = function(options)
-			local opts = _default_opts(options)
-			opts.border = "single"
-			return opts
-		end
+        win.default_opts = function(options)
+            local opts = _default_opts(options)
+            opts.border = "single"
+            return opts
+        end
 
-		-- Global mappings
-		vim.keymap.set("n", "]d", function()
-			vim.diagnostic.goto_next()
-		end, opts, { desc = "LSP Goto next diagnostic" })
-		vim.keymap.set("n", "[d", function()
-			vim.diagnostic.goto_prev()
-		end, opts, { desc = "LSP Goto previous diagnostic" })
+        -- Global mappings
+        vim.keymap.set("n", "]d", function()
+            vim.diagnostic.goto_next()
+        end, opts, { desc = "LSP Goto next diagnostic" })
+        vim.keymap.set("n", "[d", function()
+            vim.diagnostic.goto_prev()
+        end, opts, { desc = "LSP Goto previous diagnostic" })
 
-		vim.diagnostic.config({
-			virtual_text = true,
-		})
+        vim.diagnostic.config({
+            virtual_text = true,
+        })
 
-		-- Use LspAttach autocommand to only map the following keys
-		-- after the language server attaches to the current buffer
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				-- Enable completion triggered by <c-x><c-o>
-				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        -- Use LspAttach autocommand to only map the following keys
+        -- after the language server attaches to the current buffer
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local opts = { buffer = ev.buf }
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts, { desc = "LSP Hover" })
-				-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts, { desc = "LSP Goto definition" })
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts, { desc = "LSP Goto declaration" })
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts, { desc = "LSP Rename" })
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts, { desc = "LSP Code action" })
-				vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts, { desc = "LSP Toggle signature help" })
-				vim.keymap.set(
-					"n",
-					"<leader>wa",
-					vim.lsp.buf.add_workspace_folder,
-					opts,
-					{ desc = "LSP Add workspace folder" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>wr",
-					vim.lsp.buf.remove_workspace_folder,
-					opts,
-					{ desc = "LSP Remove workspace folder" }
-				)
-				vim.keymap.set("n", "<leader>wl", function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, opts, { desc = "LSP List workspace folders" })
-			end,
-		})
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = { buffer = ev.buf }
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts, { desc = "LSP Hover" })
+                -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts, { desc = "LSP Goto definition" })
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts, { desc = "LSP Goto declaration" })
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts, { desc = "LSP Rename" })
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts, { desc = "LSP Code action" })
+                vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts, { desc = "LSP Toggle signature help" })
+                vim.keymap.set(
+                    "n",
+                    "<leader>wa",
+                    vim.lsp.buf.add_workspace_folder,
+                    opts,
+                    { desc = "LSP Add workspace folder" }
+                )
+                vim.keymap.set(
+                    "n",
+                    "<leader>wr",
+                    vim.lsp.buf.remove_workspace_folder,
+                    opts,
+                    { desc = "LSP Remove workspace folder" }
+                )
+                vim.keymap.set("n", "<leader>wl", function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts, { desc = "LSP List workspace folders" })
+            end,
+        })
 
-		local lspconfig = require("lspconfig")
+        local lspconfig = require("lspconfig")
 
-		-- if you just want default config for the servers then put them in a table
-		local servers = {
-			"rust_analyzer",
-			"lua_ls",
-			-- "marksman",
-			-- "dockerls",
-			-- "terraformls",
-		}
+        -- if you just want default config for the servers then put them in a table
+        local servers = {
+            "rust_analyzer",
+            "lua_ls",
+            -- "marksman",
+            -- "dockerls",
+            -- "terraformls",
+        }
 
-		for _, lsp in ipairs(servers) do
-			lspconfig[lsp].setup({
-				capabilities = capabilities,
-			})
-		end
+        for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup({
+                capabilities = capabilities,
+            })
+        end
 
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			settings = {
-				python = {
-					checkOnType = false,
-					diagnostics = true,
-					inlayHints = true,
-					smartCompletion = true,
-					analysis = {
-						autoImportCompletions = true,
-						typeCheckingMode = "off",
-						autoSearchPaths = true,
-						useLibraryCodeForTypes = true,
-						diagnosticMode = "workspace", -- "openFilesOnly",
-					},
-				},
-			},
-		})
+        lspconfig.pyright.setup({
+            capabilities = capabilities,
+            settings = {
+                python = {
+                    checkOnType = false,
+                    diagnostics = true,
+                    inlayHints = true,
+                    smartCompletion = true,
+                    analysis = {
+                        autoImportCompletions = true,
+                        typeCheckingMode = "off",
+                        autoSearchPaths = true,
+                        useLibraryCodeForTypes = true,
+                        diagnosticMode = "workspace", -- "openFilesOnly",
+                    },
+                },
+            },
+        })
 
-		lspconfig.gopls.setup({
-			on_attach = function(_, bufnr) end,
-			capabilities = capabilities,
-
-			settings = {
-				gopls = {
-					analyses = {
-						shadow = true,
-						unusedparams = true,
-						useany = true,
-						unusedvariable = true,
-					},
-					hints = {
-						assignVariableTypes = true,
-						compositeLiteralFields = true,
-						compositeLiteralTypes = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						parameterNames = true,
-						rangeVariableTypes = true,
-					},
-					staticcheck = true,
-					gofumpt = true,
-				},
-			},
-		})
-	end,
+        lspconfig.gopls.setup({
+            capabilities = capabilities,
+            settings = {
+                gopls = {
+                    analyses = {
+                        shadow = true,
+                        unusedparams = true,
+                        useany = true,
+                        unusedvariable = true,
+                    },
+                    hints = {
+                        assignVariableTypes = true,
+                        compositeLiteralFields = true,
+                        compositeLiteralTypes = true,
+                        constantValues = true,
+                        functionTypeParameters = true,
+                        parameterNames = true,
+                        rangeVariableTypes = true,
+                    },
+                    staticcheck = true,
+                    gofumpt = true,
+                },
+            },
+        })
+    end,
 }
