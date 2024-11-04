55bb5c2aca4d854e32ea827c8978d1badad59041
Jason Merten <jmerten@salesforce.com>
2024-11-04 17:25:26 -0500

    minor changes to nvim settings

--- nvim/lua/user/settings.lua
+++ nvim/lua/user/settings.lua
@@ -1,26 +1,26 @@
 local opt = vim.opt
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+-- opt.tabstop = 2
+-- opt.softtabstop = 2
+-- opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
-opt.timeoutlen = 300      -- Time in ms to wait for a mapped sequence to complete
-opt.termguicolors = true  -- Enable colors in terminal
-opt.hlsearch = true       --Set highlight on search
+opt.timeoutlen = 300 -- Time in ms to wait for a mapped sequence to complete
+opt.termguicolors = true -- Enable colors in terminal
+opt.hlsearch = true --Set highlight on search
 opt.incsearch = true
-opt.number = true         --Make line numbers default
+opt.number = true --Make line numbers default
 opt.relativenumber = true --Make relative number default
-opt.mouse = ""            --Enable mouse mode
-opt.breakindent = true    --Enable break indent
-opt.undofile = true       --Save undo history
-opt.ignorecase = true     --Case insensitive searching unless /C or capital in search
-opt.smartcase = true      -- Smart case
+opt.mouse = "" --Enable mouse mode
+opt.breakindent = true --Enable break indent
+opt.undofile = true --Save undo history
+opt.ignorecase = true --Case insensitive searching unless /C or capital in search
+opt.smartcase = true -- Smart case
 opt.smartindent = true
 opt.expandtab = true
-opt.updatetime = 250          --Decrease update time
-opt.signcolumn = "yes"        -- Always show sign column
+opt.updatetime = 250 --Decrease update time
+opt.signcolumn = "yes" -- Always show sign column
 opt.clipboard = "unnamedplus" -- Access system clipboard
 opt.wrap = true
 
@@ -30,4 +30,3 @@ opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
 opt.scrolloff = 8
-
