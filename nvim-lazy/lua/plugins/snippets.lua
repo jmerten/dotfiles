a31222f56dde9c0b9d01b205e09cd2542073315e
Jason Merten <jmerte@gmail.com>
2024-11-05 20:47:47 -0500

    added snippets plugin file to nvim-lazy

---
+++ nvim-lazy/lua/plugins/snippets.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "garymjr/nvim-snippets",
+    opts = {
+      friendly_snippets = true,
+      search_paths = {
+        vim.fn.stdpath "config" .. "/snippets"
+      }
+    },
+  }
+}
