83f815b4062f425896b42e510e4f17ed49f8609f
Jason Merten <jmerten@salesforce.com>
2024-11-19 16:55:15 -0500

    updated nvim-lazy config

--- nvim-lazy/lua/plugins/cmp.lua
+++ nvim-lazy/lua/plugins/cmp.lua
@@ -1,92 +1,102 @@
 local cmp = require("cmp")
 local function border(hl_name)
-  return {
-    { "╭", hl_name },
-    { "─", hl_name },
-    { "╮", hl_name },
-    { "│", hl_name },
-    { "╯", hl_name },
-    { "─", hl_name },
-    { "╰", hl_name },
-    { "│", hl_name },
-  }
+	return {
+		{ "╭", hl_name },
+		{ "─", hl_name },
+		{ "╮", hl_name },
+		{ "│", hl_name },
+		{ "╯", hl_name },
+		{ "─", hl_name },
+		{ "╰", hl_name },
+		{ "│", hl_name },
+	}
 end
 
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "onsails/lspkind.nvim",
-    },
-    opts = function(_, opts)
-      opts.window = {
-        completion = {
-          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-          scrollbar = false,
-          col_offset = -3,
-          side_padding = 0,
-        },
-        documentation = {
-          border = border("CmpDocBorder"),
-          winhighlight = "Normal:CmpDoc",
-        },
-      }
-      opts.mapping = {
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-u>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<Esc>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true,
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-            -- cmp.confirm { select = true }
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }
-      opts.formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-          local strings = vim.split(kind.kind, "%s", { trimempty = true })
-          kind.kind = " " .. (strings[1] or "") .. " "
-          kind.menu = "   " .. (strings[2] or "") .. ""
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"onsails/lspkind.nvim",
+		},
+		opts = function(_, opts)
+			opts.enabled = function()
+				-- disable completion in comments
+				local context = require("cmp.config.context")
+				-- keep command mode completion enabled when cursor is in a comment
+				if vim.api.nvim_get_mode().mode == "c" then
+					return true
+				else
+					return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+				end
+			end
+			opts.window = {
+				completion = {
+					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+					scrollbar = false,
+					col_offset = -3,
+					side_padding = 0,
+				},
+				documentation = {
+					border = border("CmpDocBorder"),
+					winhighlight = "Normal:CmpDoc",
+				},
+			}
+			opts.mapping = {
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-u>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<Esc>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+						cmp.select_next_item()
+					-- cmp.confirm { select = true }
+					elseif vim.snippet.active({ direction = 1 }) then
+						vim.schedule(function()
+							vim.snippet.jump(1)
+						end)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif vim.snippet.active({ direction = -1 }) then
+						vim.schedule(function()
+							vim.snippet.jump(-1)
+						end)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}
+			opts.formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+					kind.kind = " " .. (strings[1] or "") .. " "
+					kind.menu = "   " .. (strings[2] or "") .. ""
 
-          return kind
-        end,
-      }
-      opts.view = {
-        entries = { name = "custom", selection_order = "near_cursor" },
-      }
-      opts.sources = cmp.config.sources({
-        { name = "snippets" },
-        { name = "nvim_lsp" },
-        { name = "path" },
-      }, {
-        { name = "buffer", keyword_length = 3 },
-      })
-      opts.experimental = {}
-    end,
-  },
+					return kind
+				end,
+			}
+			opts.view = {
+				entries = { name = "custom", selection_order = "near_cursor" },
+			}
+			opts.sources = cmp.config.sources({
+				{ name = "snippets" },
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+			}, {
+				{ name = "buffer", keyword_length = 3 },
+			})
+			opts.experimental = {}
+		end,
+	},
 }
