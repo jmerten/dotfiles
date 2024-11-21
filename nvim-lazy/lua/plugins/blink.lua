d9efd8d0184bda6eb592f0d89c3e1360684f6c4d
Jason Merten <jmerte@gmail.com>
2024-11-21 08:29:06 -0500

    removed lsp capability configuration from blink

--- nvim-lazy/lua/plugins/blink.lua
+++ nvim-lazy/lua/plugins/blink.lua
@@ -33,15 +33,4 @@ return {
       }
     }
   },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'saghen/blink.cmp' },
-    config = function(_, opts)
-      local lspconfig = require('lspconfig')
-      for server, config in pairs(opts.servers or {}) do
-        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-    end
-  }
 }
